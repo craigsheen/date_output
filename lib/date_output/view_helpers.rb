a77f5ed68c3b3a59a452b273ee9c909727964e11
Craig Sheen <csheen@recltd.co.uk>
2013-03-28 15:13:46 +0000

    Another slight helper fix

--- lib/date_output/view_helpers.rb
+++ lib/date_output/view_helpers.rb
@@ -1,27 +1,27 @@
 module DateOutput
   module ViewHelpers
     
-    def full_date_with_time(date,options)
+    def full_date_with_time(date,options={})
       DateOutput.full_date_with_time(date,options)
     end
 
-    def short_date_with_time(date,options)
+    def short_date_with_time(date,options={})
       DateOutput.short_date_with_time(date,options)
     end
 
-    def numbered_date_with_time(date,options)
+    def numbered_date_with_time(date,options={})
       DateOutput.numbered_date_with_time(date,options)
     end
 
-    def numbered_date(date,options)
+    def numbered_date(date,options={})
       DateOutput.numbered_date(date,options)
     end
 
-    def full_date(date,options)
+    def full_date(date,options={})
       DateOutput.full_date(date,options)
     end
 
-    def short_date(date,options)
+    def short_date(date,options={})
       DateOutput.short_date(date,options)
     end
     
