c1ef7d11a310514146b04c1067348459ded6881b
Craig Sheen <csheen@recltd.co.uk>
2013-03-28 15:28:26 +0000

    remove unsetting of options

--- lib/date_output.rb
+++ lib/date_output.rb
@@ -3,32 +3,32 @@ require "date_output/version"
 
 module DateOutput
   
-  def self.full_date_with_time(date, options={})
+  def self.full_date_with_time(date, options)
     set_options(options)
     date.strftime("%A #{date.day.ordinalize} %B %Y %H:%M%P")
   end
   
-  def self.short_date_with_time(date, options={})
+  def self.short_date_with_time(date, options)
     set_options(options)
     date.strftime("%a #{date.day.ordinalize} %b %y %H:%M%P")
   end
   
-  def self.numbered_date_with_time(date, options={})
+  def self.numbered_date_with_time(date, options)
     set_options(options)
     date.strftime("%d#{@seperator}%m#{@seperator}%Y %H:%M%P")
   end
   
-  def self.numbered_date(date, options={})
+  def self.numbered_date(date, options)
     set_options(options)
     date.strftime("%d/%m/%Y")
   end
   
-  def self.full_date(date, options={})
+  def self.full_date(date, options)
     set_options(options)
     date.strftime("%A #{date.day.ordinalize} %B %Y")
   end
   
-  def self.short_date(date, options={})
+  def self.short_date(date, options)
     set_options(options)
     date.strftime("%a #{date.day.ordinalize} %b %y")
   end
